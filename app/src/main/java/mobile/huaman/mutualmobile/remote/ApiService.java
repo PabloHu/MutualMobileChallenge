b72b4474951bf81b8a38bd182e292116d1ab4d63
PabloHu <pchuaman@gmail.com>
2017-12-28 14:21:31 -0500

    Update ApiService.java

--- app/src/main/java/mobile/huaman/mutualmobile/remote/ApiService.java
+++ app/src/main/java/mobile/huaman/mutualmobile/remote/ApiService.java
@@ -14,25 +14,11 @@ import retrofit2.http.Query;
 
 public interface ApiService {
 
-    //@GET("evolution-chain/{id}")
-    // Observable<PokemonEvolutionChainResponse> getPokemonEvolutionChainResponse(@Path("id")String pokemon);
-
-   // @GET("pokemon/?limit=801&offset=0")
-   // Observable<PokemonListResponse> getPokemonListResponse();
-
-    //@GET("pokemon/?limit=356&offset=0")
-    //Observable<PokemonListResponse> getPokemonListResponse();
-
-
- //   @GET("search?q={recipeSearch}&app_id=e124706a&app_key=581332a6fc9bf6939343b66770f38718")
- //   Observable<RecipeResponse> getRecipeResponse(@Path("recipeSearch")String recipeSearch);
-
+   
 
     @GET("search?")
     Observable<RecipeResponse> getRecipeResponse(
             @Query("q") String search, @Query("app_id") String app_id, @Query("app_key") String app_key);
 
-    //@GET("v1/gifs/random?")
-    //Observable<RecipeResponse> getRecipeResponse(
-     //       @Query("api_key") String key, @Query("tag") String tag, @Query("rating") String rating);
+ 
 }
